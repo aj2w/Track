cbf5107e31ec37e5b937015900f61cb97c71b3ee
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-09-04 14:20:38 -0400

    Added smtp gmail settings to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,17 +78,17 @@ SubwayStatus::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  ### EDIT ALL OF THE BELOW FOR MAILING IN PRODUCTION ###
+  ### BELOW IS SET UP FOR MAILING IN PRODUCTION ###
 
-  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-#   ActionMailer::Base.delivery_method = :smtp
-# ActionMailer::Base.smtp_settings = {
-#    :tls => true,
-#    :address => "smtp.gmail.com",
-#    :port => 587,
-#    :domain => "gmail.com",
-#    :authentication => :login,
-#    :user_name => "[username]",
-#    :password => "[password]"
- }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  # :domain               => 'gmail.com',
+  :user_name            => ENV['GMAIL_USERNAME'],
+  :password             => ENV['GMAIL_PASSWORD'],
+  :authentication       => 'plain',
+  :enable_starttls_auto => true
+}
+
 end
