683615761f42a0e1e0ed08a995a930ad888b8180
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-09-05 13:36:53 -0400

    Added subway images and set up status page - going to add the images to each row

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,6 +8,21 @@ class WelcomeController < ApplicationController
     @alerts = Alert.where(:user_id == current_user.id)
   end
 
+  def status
+    @line123 = Train.find(1)
+    @line456 = Train.find(2)
+    @line7 = Train.find(3)
+    @lineACE = Train.find(4)
+    @lineBDFM = Train.find(5)
+    @lineG = Train.find(6)
+    @lineJZ = Train.find(7)
+    @lineL = Train.find(8)
+    @lineNQR = Train.find(9)
+    @lineS = Train.find(10)
+    @lineSIR = Train.find(11)
+  end
+
+
 private
   #This is all my privates below
   def alert_params
