a9821aa52af4db54ee8a16ba93aa15d475cc3171
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-12-17 11:59:49 -0500

    Removed new feature language from mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,11 @@ class UserMailer < ActionMailer::Base
   default from: "track.automailer@gmail.com"
 
   def selected_status(user, alert)
-    @greeting = "Hi there"
+    # @greeting = "Hi"
     @user = user
     @alert = alert
     @train = alert.train
 
-    mail to: user.email, subject: "[TRACK] #{@train.name } line status: #{@train.status} - #{Date.today.strftime("%D")} *NEW FEATURE INFO INSIDE*"
+    mail to: user.email, subject: "[TRACK] #{@train.name }: #{@train.status} - #{Date.today.strftime("%D")}"
   end
 end
